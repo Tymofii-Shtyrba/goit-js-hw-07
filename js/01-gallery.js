9dacddc88526726580dd9705fd70066e93185831
Tymofii-Shtyrba <timofeyshtyrba@gmail.com>
2023-02-25 21:48:49 +0200

    First version of the work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContrainerRef = document.querySelector('.gallery');
+
+const createMarkup = function (markupContent) {
+  return markupContent.map(contentItem =>
+    `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img
+    class="gallery__image"
+    src="${contentItem.preview}"
+    data-source="${contentItem.original}"
+    alt="${contentItem.description}"
+    />
+    </a>
+    </div>`).join('');
+}
+
+galleryContrainerRef.innerHTML = createMarkup(galleryItems);
+
+const hendleClick = event => {
+    if (event.target.nodeName !== 'IMG') {
+      return;
+    }
+    
+    event.preventDefault();
+  
+  const largeImageSource = event.target.getAttribute('data-source');
+  const largeImageAlt = event.target.getAttribute('alt');
+  const instance = basicLightbox.create(`<img src="${largeImageSource}" alt="${largeImageAlt}">`);
+
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+  instance.show();
+};
+
+galleryContrainerRef.addEventListener('click', hendleClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContrainerRef = document.querySelector('.gallery');
+
+const createMarkup = function (markupContent) {
+  return markupContent.map(contentItem =>
+    `<a class="gallery__item" href="${contentItem.original}">
+      <img class="gallery__image" src="${contentItem.preview}" alt="${contentItem.description}" />
+    </a>`).join('');
+}
+
+galleryContrainerRef.innerHTML = createMarkup(galleryItems);
+
+lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
